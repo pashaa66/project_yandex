aa5d579350d5233d30eac09536c88ae40c3fe8c5
Pavel <pasha10428@gmail.com>
2024-01-06 15:58:16 +0300

    Updated main.py(added start screen), created level_builder.py

---
+++ level_builder.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import pygame
+
+
+
+class LevelBuilder:
+    def __init__(self):
+        self.filename = 'map.txt'
+
+    def load_level(self):
+        try:
+            filename = "data/" + self.filename
+            with open(filename, 'r') as mapFile:
+                level_map = [line.strip() for line in mapFile]
+                max_width = max(map(len, level_map))
+            return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+        except FileNotFoundError as e:
+            print(e)
+
+    def load_image(self, name, colorkey=None):
+        fullname = os.path.join('data', name)
+        if not os.path.isfile(fullname):
+            print(f"Файл с изображением '{fullname}' не найден")
+            sys.exit()
+        image = pygame.image.load(fullname)
+        if colorkey is not None:
+            image = image.convert()
+            if colorkey == -1:
+                colorkey = image.get_at((0, 0))
+            image.set_colorkey(colorkey)
+        else:
+            image = image.convert_alpha()
+        return image
+
--- main.py
+++ main.py
@@ -2,25 +2,66 @@ import os
 
 import pygame
 import sys
+from level_builder import LevelBuilder
 
 pygame.init()
-BLACK = pygame.Color("#000000")
-SIZE = WIDTH, HEIGHT = (550, 550)
-FPS = 50
-screen = pygame.display.set_mode(SIZE)
-clock = pygame.time.Clock()
+
+class EscapeFromLesok:
+    def __init__(self):
+        self.FPS = 50
+        self.size = SIZE = WIDTH, HEIGHT = (800, 800)
+        self.BLACK = pygame.Color("#000000")
+        self.WHITE = pygame.Color("white")
+        self.RED = pygame.Color("red")
+        self.running = True
+        self.l_b = LevelBuilder()
+        self.screen = pygame.display.set_mode(SIZE)
+        self.clock = pygame.time.Clock()
+
+    def terminate(self):
+        pygame.quit()
+        sys.exit()
+
+    def start_screen(self):
+        intro_text = ["Побег из Леска", "",
+                      "Правила игры:",
+                      "Находите батарейки, чтобы фонарик не погас",
+                      "Остерегайтесь темноты"]
+
+        fon = pygame.transform.scale(self.l_b.load_image('fon.jpg'), self.size)
+        self.screen.blit(fon, (0, 0))
+        font = pygame.font.Font(None, 30)
+        text_coord = 50
+        for line in intro_text:
+            string_rendered = font.render(line, 1, self.RED)
+            intro_rect = string_rendered.get_rect()
+            text_coord += 10
+            intro_rect.top = text_coord
+            intro_rect.x = 10
+            text_coord += intro_rect.height
+            self.screen.blit(string_rendered, intro_rect)
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    return
+            pygame.display.flip()
+            self.clock.tick(self.FPS)
+
+    def run_game(self):
+        self.start_screen()
+        while self.running:
+            self.clock.tick(self.FPS)
+            self.screen.fill(self.BLACK)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+            pygame.display.flip()
+        self.terminate()
 
 
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-running = True
-while running:
-    clock.tick(FPS)
-    screen.fill(BLACK)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-terminate()
+game = EscapeFromLesok()
+game.run_game()
